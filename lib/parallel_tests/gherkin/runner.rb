2654b8eb4ac1231f1d25a01f737a08771269271b
ejoubaud <emmanuel.joubaud@envato.com>
2013-09-12 09:55:41 +1000

    Maybe we want those runtime logger after all...

--- lib/parallel_tests/gherkin/runner.rb
+++ lib/parallel_tests/gherkin/runner.rb
@@ -11,8 +11,8 @@ module ParallelTests
           options = options.merge(:env => {"AUTOTEST" => "1"}) if $stdout.tty? # display color when we are in a terminal
           cmd = [
             executable,
+            (runtime_logging if File.directory?(File.dirname(runtime_log))),
             '--expand', '--format Teamcity::Cucumber::Formatter',
-            # (runtime_logging if File.directory?(File.dirname(runtime_log))),
             cucumber_opts(options[:test_options]),
             *sanitized_test_files
           ].compact.join(" ")
